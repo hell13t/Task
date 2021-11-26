3578fde913f5fce54a549fcdbe16bddef0f439fb
Adlet <66587129+hell13t@users.noreply.github.com>
2021-11-26 23:25:11 +0600

    Update ParserCode.py

--- ParserCode.py
+++ ParserCode.py
@@ -7,30 +7,22 @@ from bs4 import BeautifulSoup as BS
 x = PrettyTable()
 
 Place = []
-Сases = []
+lis = []
 
-
-x.add_column("Местоположения",[Place])
-x.add_column("Все случаи заболевания   Новые случаи за 1дн.   Умерло", [Сases])
-
+x.add_column("Местоположения   Все случаи заболевания   Новые случаи за 1дн.   Умерло",[Place])
 
 r = requests.get('https://news.google.com/covid19/map?hl=ru&gl=RU&ceid=RU%3Aru')
 
 html = BS(r.content, 'lxml')
-
-lis = []
-
 
 table = html.find('tbody', class_ = 'ppcUXd').find_all('tr') #Полная Таблица
 
 
 for item in table:
 	item_text = item.text
-	print(item_text)
-
-table2 = html.find('tbody', class_ = 'ppcUXd').find_all('th') #Таблица названий стран
+	lis.append(item_text)
 
 
-for item in table2:
-	item_text = item.text
-	Place.append(item_text)
+Place.append(random.choice(lis))
+
+print(x)
