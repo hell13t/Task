878d2f2902900c533e9f46e7f6eb7f3b78283a3d
Adlet <66587129+hell13t@users.noreply.github.com>
2021-11-27 04:46:01 +0600

    Update ParserCode.py
    Done!

--- ParserCode.py
+++ ParserCode.py
@@ -1,28 +1,41 @@
 from prettytable import PrettyTable
+import unidecode
 import random
-
 import requests
 from bs4 import BeautifulSoup as BS
 
+a = random.randint(0,40)
 x = PrettyTable()
-
 Place = []
-lis = []
-
-x.add_column("Местоположения   Все случаи заболевания   Новые случаи за 1дн.   Умерло",[Place])
+name = []
+data = []
 
 r = requests.get('https://news.google.com/covid19/map?hl=ru&gl=RU&ceid=RU%3Aru')
-
 html = BS(r.content, 'lxml')
 
-table = html.find('tbody', class_ = 'ppcUXd').find_all('tr') #Полная Таблица
+x.add_column("     Местоположения     |     Все случаи заболевания     |     Новые случаи за 1дн.     |     Кол-во на млн ч.     |     Умерло     ",[Place])
+
+tableCountries = html.find('tbody', class_ = 'ppcUXd').find_all('th') #Названия стран
+tableData = html.find('tbody', class_ = 'ppcUXd').find_all('td') #Данные
+
+for i in tableCountries:
+    i_text = i.text
+    name.append(i_text)
+
+for j in tableData:
+    j_text = j.text
+    data.append([j_text])
 
 
-for item in table:
-	item_text = item.text
-	lis.append(item_text)
+resultData = [[e for sublist in data[i:i+5] for e in sublist] for i in range(0,len(data),5)]
 
+nameStr = ''.join(name[a])
+dataStr = ','.join(resultData[a])
+dataStr = dataStr.replace(',,', ',')
+dataStr = dataStr.replace(',', '               ')
+dataStr = unidecode.unidecode(dataStr)
+result = nameStr + '               ' + dataStr
 
-Place.append(random.choice(lis))
+Place.append(result)
 
 print(x)
